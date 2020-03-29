e0b2d2038a2f8ee2c6a70795f66acc249586aff9
JuanHDSM <juanholydesouza99@gmail.com>
2020-03-29 02:17:55 -0300

    Front-end do app

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const OngController = require('./controllers/OngController');
-const IncidentController = require('./controllers/IncidentController');
-const ProfileController = require('./controllers/ProfileController');
-const SessionController = require('./controllers/SessionController');
+const OngController = require('./controllers/OngControllers');
+const IncidentController = require('./controllers/IncidentsControllers');
+const ProfileController = require('./controllers/ProfileControllers');
+const SessionController = require('./controllers/SessionControllers');
 
 const routes = express.Router();
 
